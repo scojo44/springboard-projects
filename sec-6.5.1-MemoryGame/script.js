2ee575eb68dcc336d0973e4fb1db116fd12fdb3c
scojo44 <Scott@scojo.org>
2023-08-15 17:38:48 -0700

    Show a score as a percentage of tries

--- sec-6.5.1-MemoryGame/script.js
+++ sec-6.5.1-MemoryGame/script.js
@@ -91,12 +91,11 @@ function onCardClick(e) {
   if(guesses.length >= MAX_GUESSES) {
     // Check for matching colors
     const match = guesses[0].dataset.color === guesses[1].dataset.color;
-    incrementIndicator(document.getElementById("tries"));
 
     if(match){
       resetGuesses();
       // Update match count
-      incrementIndicator(document.getElementById("matches"));
+      matchCount++;
       isGameOver();
     } else {
       showingMismatchedCards = true;
@@ -109,6 +108,9 @@ function onCardClick(e) {
         resetGuesses();
       }, 1000)
     }
+
+    tryCount++;
+    updateIndicators();
   }
 }
 
@@ -120,8 +122,14 @@ function isGameOver(){
   }
 }
 
-function incrementIndicator(counter){
-  counter.innerText = parseInt(counter.innerText) + 1;
+let matchCount = 0
+let tryCount = 0;
+
+function updateIndicators(){
+  document.getElementById("matches").innerText = matchCount;
+  document.getElementById("tries").innerText = tryCount;
+  if(tryCount > 0)
+    document.getElementById("score").innerText = Math.round(matchCount/tryCount*100) + "%";
 }
 
 document.getElementById("start").addEventListener("click", function(e){
@@ -133,6 +141,8 @@ document.getElementById("start").addEventListener("click", function(e){
 document.getElementById("new-game").addEventListener("click", function(e){
   setupGameboard(); // Flip all the cards back over and shuffle them
   e.target.style.display = "none";
+  matchCount = 0;
+  tryCount = 0;
 });
 
 const CHEAT_CODE = "xyzzy";
