6835bd316ccaaaddcaec73d6b8377b96cdda5f6f
scojo44 <Scott@scojo.org>
2024-06-20 13:00:42 -0700

    37.2.1 Start Express Jobly
    - List routes at startup
    - Better config list for running tests at startup
    - Format code a bit

---
+++ sec-37.2.1-ExpressJobly/app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/** Express app for jobly. */
+
+const express = require("express");
+const expressListRoutes = require('express-list-routes');
+const morgan = require("morgan");
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+const { authenticateJWT } = require("./middleware/auth");
+
+const authRoutes = require("./routes/auth");
+const companiesRoutes = require("./routes/companies");
+const usersRoutes = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/companies", companiesRoutes);
+app.use("/users", usersRoutes);
+
+if(process.env.NODE_ENV != 'test') {
+  console.log('===== Route List =====================');
+  expressListRoutes(app, { color: true });
+  console.log('======================================');
+}
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+module.exports = app;
