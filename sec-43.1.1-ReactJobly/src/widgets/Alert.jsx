fc59c5a768471f3c31cde0027a427b3cd39a61b3
scojo44 <Scott@scojo.org>
2024-08-12 16:17:21 -0700

    43.1.1 Improve Alert component

--- sec-43.1.1-ReactJobly/src/widgets/Alert.jsx
+++ sec-43.1.1-ReactJobly/src/widgets/Alert.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Alert.css'
 
-export default function Alert({type, messages = []}) {
+export default function Alert({alerts, dismiss}) {
   return (
-    <aside className={`Alert ${type}`}>
-      {messages.map((m,i) => <p key={i}>{m}</p>)}
+    <aside className="Alert">
+      {alerts.map((alert, i) =>
+        <p className={alert.category} key={i}>
+          <span>{alert.message}</span>
+          <button type="button" onClick={() => dismiss(alert)}>X</button>
+        </p>
+      )}
     </aside>
   );
 }
